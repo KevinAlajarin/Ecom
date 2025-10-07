85f165731d45bc59f1b99321b032de6acbf25121
Kevin <alajarinkevin@hotmail.com>
2025-10-07 14:35:08 -0300

    Update vite.config.js with base URL and rebuild dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ecom/' // Añade esta línea (nombre exacto del repositorio)
 })
