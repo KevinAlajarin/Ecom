3453ce46756f5c0a823a61ff2c1261848e69f79c
Kevin <alajarinkevin@hotmail.com>
2025-10-07 15:45:00 -0300

    edit vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ecom/' // Añade esta línea (nombre exacto del repositorio)
+  base: '/Ecom/dist/' // Cambia a /Ecom/dist/ para que los recursos se carguen desde la subcarpeta dist
 })
